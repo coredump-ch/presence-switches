da8074a811ca782e979bf3ca5a8d8ea3b4c773c2
Danilo Bargen <gezuru@gmail.com>
2013-11-27 23:07:39 +0100

    Display number on 7 segment display

--- src/main.ino
+++ src/main.ino
@@ -1,33 +1,53 @@
 #include <Wire.h>
+#include "TM1637.h"
 
-const uint8_t I2C_ADDRESS = 0x20;
+#define I2C_ADDRESS 0x20
+#define DISP_CLK 2
+#define DISP_DIO 3
+#define DISP_BRIGHTNESS 4
 
 uint8_t lower, upper, number;
 
+TM1637 tm1637(DISP_CLK, DISP_DIO);
+int8_t disp[] = {0x00, 0x00, 0x00, 0x00};
+
 void setup() {
-    pinMode(13, OUTPUT);
+    // Initialize I2C
+    Wire.begin();
+
+    // Initialize display
+    tm1637.set(DISP_BRIGHTNESS);
+    tm1637.init();
+    tm1637.clearDisplay();
 
     // Initialize serial console
     Serial.begin(9600);
-
-    // Initialize I2C
-    Wire.begin();
 }
 
 void loop() {
     Wire.requestFrom(I2C_ADDRESS, 2);
 
-    // Get lower and upper bytes from i2c
+    // Get lower and upper bytes from I²C
     lower = ~Wire.read();
     upper = ~Wire.read();
 
-    // We only need the lower part (8 switches)
-    number = (uint8_t)lower;
+    // We only need the lower part (8 switches).
+    // Make sure number has only 4 digits.
+    number = lower % 10000;
+
+    // Get the four digits
+    disp[0] = number / 1000;
+    disp[1] = number % 1000 / 100;
+    disp[2] = number % 100 / 10;
+    disp[3] = number % 10;
+
+    // Display
+    tm1637.display(disp);
 
     // Log output
     Serial.print("Number of people present: ");
     Serial.print(number);
     Serial.print("\r\n");
 
-    delay(2000);
+    delay(20);
 }
